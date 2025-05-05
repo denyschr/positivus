891b8a94ed4721b0fccc34bf857de6ae06a51c97
denyschr <denprogman99@gmail.com>
2025-05-05 12:02:17 +0300

    refactor: simplify CSS class names

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,17 +3,19 @@ import { debounce } from "./helpers.js";
 export default function initNavbar() {
   const body = document.body;
   const navbar = document.querySelector("[data-navbar]");
-  const toggleButton = document.querySelector("[data-navbar-toggle-button]");
+  const toggleMenuButton = document.querySelector(
+    "[data-navbar-toggle-menu-button]"
+  );
 
   const toggleMenu = () => {
     body.classList.toggle("lock");
     body.classList.contains("menu-open")
-      ? toggleButton.setAttribute("aria-expanded", "false")
-      : toggleButton.setAttribute("aria-expanded", "true");
+      ? toggleMenuButton.setAttribute("aria-expanded", "false")
+      : toggleMenuButton.setAttribute("aria-expanded", "true");
     body.classList.toggle("menu-open");
   };
 
-  toggleButton.addEventListener("click", toggleMenu);
+  toggleMenuButton.addEventListener("click", toggleMenu);
 
   window.addEventListener("scroll", () => {
     window.scrollY > 0
@@ -27,7 +29,7 @@ export default function initNavbar() {
       if (window.innerWidth >= 992) {
         body.classList.remove("lock");
         body.classList.remove("menu-open");
-        toggleButton.setAttribute("aria-expanded", "false");
+        toggleMenuButton.setAttribute("aria-expanded", "false");
       }
     })
   );
